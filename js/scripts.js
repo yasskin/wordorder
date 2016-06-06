2ebed85768251d59982469311dd22d615bc26012
Charles Ewel and Noah Yasskin <charles@>
2016-06-06 16:35:13 -0700

    fix submit button such that it clears prior output

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // business logic
-var textinput;
-var arrayinputs = [];
-var uniquearrays = [];
-var wordtotals = [];
-var uniquetwoarrays = [];
+// var textinput;
+// var arrayinputs = [];
+// var uniquearrays = [];
+// var wordtotals = [];
+// var uniquetwoarrays = [];
 function checkAvailability(arr, val){
   return arr.some(function(arrVal){
     return val === arrVal;
@@ -18,13 +18,18 @@ function comparisonCheck(arr1, arr2) {
   }
 };
 
-
+//user interface or front-end logic
 //input a block of text and
 $(document).ready(function() {
   $("form#wordorderform").submit(function() {
+    var textinput;
+    var arrayinputs = [];
+    var uniquearrays = [];
+    var wordtotals = [];
+    var uniquetwoarrays = [];
+     $("ul#outputlist li").remove();
     textinput = $("input#textinput").val();
     arrayinputs = textinput.split([" "]);
-    //arrayinputs = arrayinputs.sort();
     //and return how many times the words appeared.
     arrayinputs.forEach(function(arrayinput1){
       var total = 0;
@@ -50,10 +55,6 @@ $(document).ready(function() {
     uniquetwoarrays.forEach(function(arr) {
       $("#outputlist").append("<li>" + "word(col1): " + arr[0] + " #times(col2): " + arr[1] + " first appearance/index(col3): " + arr[2] + "</li>");
     });
-    console.log(uniquetwoarrays);
-
   event.preventDefault();
   });
 });
-
-//If you finish that are you're feeling really comfortable with forEach() loops, try to do do the above exercise but order the list by the number of appearances - greatest to least. For words that appear the same amount of times, order by which word appeared first.
